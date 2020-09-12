c0fbc75935b87efcecd8de6c86a83848f16cb2d1
JBrickhouse <jbrickhouse777@gmail.com>
2020-09-11 19:53:56 -0500

    html routes implemented

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,22 @@
+
+// Path package required to get the correct file paths for the html files
+var path = require("path");
+
+// ROUTING
+
+// Exporting the HTML GET requests
+module.exports = function (app) {
+
+    // The code below handles when users "visit" a page
+    // In each of the cases below, a user is show the HTML content
+
+    // If the route is /notes, display the notes.html
+    app.get("/notes", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
+    // If no matching route is found, default displaying to the index.html
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+}
---
+++ server.js
@@ -0,0 +1,35 @@
+
+//----------------------------------------------------
+// Dependencies
+// Express npm package required to run the Express server
+var express = require("express");
+
+//----------------------------------------------------
+// Express configuration
+// Establishing the basic properties of our Express server
+
+// Tells node.js to create an Express server
+var app = express();
+
+// Sets an initial port to be used later in the listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//----------------------------------------------------
+// Routing
+// Pointing the server to a collection of route files
+// The routes serve as a map
+// And the map describes how to respond when users visit or request data from the URLs
+
+require("./routes/htmlRoutes")(app);
+
+
+//----------------------------------------------------
+// Listener
+// This code starts the server and has it listening
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
