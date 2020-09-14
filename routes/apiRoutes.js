fc275342f6f0841ec739d0192b90eba197e8ca18
JBrickhouse <jbrickhouse777@gmail.com>
2020-09-14 15:13:11 -0500

    DELETE request succesfully working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,6 +22,7 @@ module.exports = function (app) {
     // GET request
     // Reads the db.json file and returns all the saved notes as JSON
     app.get("/api/notes", function (req, res) {
+        // Sending the db.json as the response
         res.send(db);
     });
 
@@ -35,7 +36,7 @@ module.exports = function (app) {
             // Gets the note title and text from the request body
             title: req.body.title,
             text: req.body.text
-        }
+        };
 
         // Adds the new note the db.json file
         db.push(newNote);
@@ -43,11 +44,30 @@ module.exports = function (app) {
         // Sending the newNote as the response
         res.send(newNote);
     });
-
-
-
+    
     // DELETE request
+    app.delete("/api/notes/:id", function (req,res) {
 
+        // Getting the unique note ID from the request
+        // This is the ID of the note we want to delete
+        var noteID = req.params.id
+        
+        // Using forEach to go through the db.json, which is an array of objects
+        db.forEach(object => {
+            // if object.id corresponds to noteID
+            // (Meaning that the object in question is the note we want to delete)
+            if(object.id === noteID) {
+                // Use indexOf to get the index of the object within the db.json array
+                // Save it as a variable called objectIndex
+                var objectIndex = db.indexOf(object);
+                // Use splice to remove the object at that index in the db.json array
+                // (The second parameter, 1, means that we're only removing 1 item, which
+                // means that we're only removing 1 object - the note - from the array)
+                db.splice(objectIndex,1);
+            }
+        });
 
-
+        // Sending the db.json as the response
+        res.send(db);
+    })
 };
